994ab9ff02a93330f7c7344afcc3e4d45afd68ab
Lan Vukušič <lan.vukusic@gmail.com>
2023-03-06 21:37:51 +0100

    igranje z marinkom

--- prototype/branch.py
+++ prototype/branch.py
@@ -17,6 +17,12 @@ class Point(object):
         self.y /= length
         return self
 
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
 
@@ -30,11 +36,13 @@ class Point(object):
 
 BRANCHING_PROBABILITY = 0.2
 # normal branch grouwth direction
-DIRECTION_LOW = Point(10, 9).normalize()
-DIRECTION_HIGH = Point(10, -9).normalize()
+DIRECTION_LOW = Point(10, 20).normalize()
+DIRECTION_HIGH = Point(10, -20).normalize()
 
-NEW_BRANCH_DIRECTION_HIGH = Point(0, 5)
-NEW_BRANCH_DIRECTION_LOW = Point(0, -5)
+NEW_BRANCH_DIRECTION_HIGH = Point(0, 12)
+NEW_BRANCH_DIRECTION_LOW = Point(0, 5)
+
+branches = []
 
 
 def lerpPoint(a: Point, b: Point, t: float):
@@ -45,6 +53,9 @@ def create_brach(start: Point, maxLen: float, direction: Point, depth=1):
     if maxLen < 1:
         return
 
+    points = []
+
+    i = 0
     prev_point = start
     prev_direction = direction
 
@@ -53,44 +64,85 @@ def create_brach(start: Point, maxLen: float, direction: Point, depth=1):
 
     remainingLen = maxLen
     while remainingLen > 1:
+        u = i / maxLen
+
         # calculate direction
         new_direction = lerpPoint(DIRECTION_LOW, DIRECTION_HIGH, random.random())
         # new_direction.normalize()
         new_direction = (prev_direction * 0.2 + new_direction * 0.8).normalize()
 
         # calculate point
-        new_point = prev_point + new_direction * (float(remainingLen) * 5.0)
+        new_point = prev_point + new_direction * (float(remainingLen) * 8.0)
 
         # plot the branch as a line
         plt.plot(
-            [prev_point.x, new_point.x], [prev_point.y, new_point.y], color="black"
+            [prev_point.x, new_point.x],
+            [prev_point.y, new_point.y],
+            color="black",
+            linewidth=(1 - u) * 4,
         )
+        points.append(new_point)
 
         # mybe branch
-        if remainingLen > 5 and depth < 3:
+        if i > 2 and remainingLen > 10 and depth < 3:
             if BRANCHING_PROBABILITY / (math.log(depth + 0.2) * 2) > random.random():
                 branching_dir = lerpPoint(
                     NEW_BRANCH_DIRECTION_LOW, NEW_BRANCH_DIRECTION_HIGH, random.random()
                 )
+                if random.random() > 0.5:
+                    branching_dir = Point(branching_dir.x, -branching_dir.y)
+                branching_dir.normalize()
+
                 new_branch_dir = branching_dir + new_direction
                 create_brach(
-                    prev_point, int(remainingLen / 1.7), new_branch_dir, depth + 1
+                    prev_point,
+                    int(remainingLen / (depth + 0.3)),
+                    new_branch_dir,
+                    depth + 1,
                 )
 
         prev_point = new_point
         prev_direction = new_direction
         remainingLen -= 1
+        i += 1
+
+    branches.append(points)
+
+
+def flatten(points: list[Point]):
+
+    out = []
+    for point_i in range(0, len(points), 3):
+        print(point_i)
+        if point_i + 3 >= len(points):
+            break
+        out.append(points[point_i].x)
+        out.append(points[point_i].y)
+
+        out.append(points[point_i + 1].x)
+        out.append(points[point_i + 1].y)
+
+        out.append(points[point_i + 2].x)
+        out.append(points[point_i + 2].y)
+
+        out.append(points[point_i + 3].x)
+        out.append(points[point_i + 3].y)
+
+    return out
 
 
 def main():
     # plt.ion()
     # plt.show()
     while True:
-        create_brach(Point(0, 0), 20, Point(1, 0))
+        create_brach(Point(0, 0), 50, Point(1, 0))
 
         # plt.ioff()
         ax = plt.gca()
         ax.set_aspect("equal", adjustable="box")
+        print()
+        print("Branches: ")
+        print(flatten(branches[0]))
         plt.show()
 
 
