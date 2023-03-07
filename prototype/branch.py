a7f8a90b67a92b91eefbb21b9c0624d243cc1737
Lan Vukušič <lan.vukusic@gmail.com>
2023-03-07 20:43:14 +0100

    ma aaa aaa nevem

--- prototype/branch.py
+++ prototype/branch.py
@@ -26,6 +26,9 @@ class Point(object):
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
 
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
     def __mul__(self, other: float):
         if isinstance(other, Point):
             return self.x * other.x + self.y * other.y
@@ -36,8 +39,8 @@ class Point(object):
 
 BRANCHING_PROBABILITY = 0.2
 # normal branch grouwth direction
-DIRECTION_LOW = Point(10, 20).normalize()
-DIRECTION_HIGH = Point(10, -20).normalize()
+DIRECTION_LOW = Point(10, 17).normalize()
+DIRECTION_HIGH = Point(10, -17).normalize()
 
 NEW_BRANCH_DIRECTION_HIGH = Point(0, 12)
 NEW_BRANCH_DIRECTION_LOW = Point(0, 5)
@@ -49,7 +52,7 @@ def lerpPoint(a: Point, b: Point, t: float):
     return Point(a.x + (b.x - a.x) * t, a.y + (b.y - a.y) * t)
 
 
-def create_brach(start: Point, maxLen: float, direction: Point, depth=1):
+def create_brach(start: Point, maxLen: float, direction: Point, depth=1, width=0):
     if maxLen < 1:
         return
 
@@ -66,28 +69,43 @@ def create_brach(start: Point, maxLen: float, direction: Point, depth=1):
     while remainingLen > 1:
         u = i / maxLen
 
-        # calculate direction
-        new_direction = lerpPoint(DIRECTION_LOW, DIRECTION_HIGH, random.random())
-        # new_direction.normalize()
-        new_direction = (prev_direction * 0.2 + new_direction * 0.8).normalize()
+        if (i + 1) % 4 == 0 and i >= 2:
+            # mirror i-2 point over i-1 point
+            new_direction = prev_point - points[-2]
+            new_point = prev_point + (new_direction)
+            # plot point with a circle
+            plt.plot(new_point.x, new_point.y, "o", color="purple", markersize=7)
 
-        # calculate point
-        new_point = prev_point + new_direction * (float(remainingLen) * 8.0)
+        else:
+            # calculate direction
+            new_direction = lerpPoint(DIRECTION_LOW, DIRECTION_HIGH, random.random())
+            # new_direction.normalize()
+            new_direction = (prev_direction * 0.2 + new_direction * 0.8).normalize()
+
+            # calculate point
+            new_point = prev_point + new_direction * (float(remainingLen) * 8.0)
+            # plot point with a circle
+            plt.plot(new_point.x, new_point.y, "o", color="orange", markersize=7)
 
         # plot the branch as a line
         plt.plot(
             [prev_point.x, new_point.x],
             [prev_point.y, new_point.y],
-            color="black",
+            color="green",
             linewidth=(1 - u) * 4,
         )
+
         points.append(new_point)
 
+        # line width based on provided width, decreasing with i
+        width = width - 0.1
         # mybe branch
-        if i > 2 and remainingLen > 10 and depth < 3:
-            if BRANCHING_PROBABILITY / (math.log(depth + 0.2) * 2) > random.random():
+        if i > 2 and remainingLen > 10 and depth < 3 and False:
+            if BRANCHING_PROBABILITY / (math.log(depth + 0.5) * 2) > random.random():
                 branching_dir = lerpPoint(
-                    NEW_BRANCH_DIRECTION_LOW, NEW_BRANCH_DIRECTION_HIGH, random.random()
+                    NEW_BRANCH_DIRECTION_LOW,
+                    NEW_BRANCH_DIRECTION_HIGH,
+                    random.random(),
                 )
                 if random.random() > 0.5:
                     branching_dir = Point(branching_dir.x, -branching_dir.y)
@@ -99,6 +117,7 @@ def create_brach(start: Point, maxLen: float, direction: Point, depth=1):
                     int(remainingLen / (depth + 0.3)),
                     new_branch_dir,
                     depth + 1,
+                    width,
                 )
 
         prev_point = new_point
@@ -128,20 +147,20 @@ def flatten(points: list[Point]):
         out.append(points[point_i + 3].x)
         out.append(points[point_i + 3].y)
 
-    return out
+    return out[: len(out) - (len(out) - 1) % 4]
 
 
 def main():
     # plt.ion()
     # plt.show()
     while True:
-        create_brach(Point(0, 0), 50, Point(1, 0))
+        create_brach(Point(0, 0), 50, Point(1, 0), 1, 10)
 
         # plt.ioff()
         ax = plt.gca()
         ax.set_aspect("equal", adjustable="box")
         print()
-        print("Branches: ")
+        print("Branches: ", len(branches[0]))
         print(flatten(branches[0]))
         plt.show()
 
